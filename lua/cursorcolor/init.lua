d587d1efd9f10f36f86d0e05d91fcc7859bfddb4
Ellie Bonné <ellie.bonne@inform-software.com>
2024-09-24 13:59:34 +0200

    commit

--- lua/cursorcolor/init.lua
+++ lua/cursorcolor/init.lua
@@ -1,34 +1,34 @@
 local M = {}
 
 function M.setup(opts)
-	vim.opt.cursorline = true
-	local default = vim.api.nvim_get_hl(0, { name = "CursorLine" }).bg
-	if not opts.normal then
-		opts.normal = default
-	end
-	if not opts.insert then
-		opts.insert = default
-	end
+  vim.opt.cursorline = true
+  local default = vim.api.nvim_get_hl(0, { name = 'CursorLine' }).bg
+  if not opts.normal then
+    opts.normal = default
+  end
+  if not opts.insert then
+    opts.insert = default
+  end
 
-	vim.api.nvim_set_hl(0, "CursorLine", { bg = opts.normal })
+  vim.api.nvim_set_hl(0, 'CursorLine', { bg = opts.normal })
 
-	vim.api.nvim_create_autocmd("ModeChanged", {
-		callback = function()
-			if not vim.api.nvim_get_all_options_info()["modifiable"] then
-				vim.api.nvim_set_hl(0, "CursorLine", { bg = default })
-				return
-			end
-			local mode = vim.api.nvim_get_mode().mode
-			local color = default
-			-- #vim.highlight.create("CursorLine", { guibg = "#3c3836" })
-			if mode == "n" then
-				color = opts.normal
-			elseif mode == "i" then
-				color = opts.insert
-			end
-			vim.api.nvim_set_hl(0, "CursorLine", { bg = color })
-		end,
-	})
+  vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
+    callback = function(args)
+      if not vim.api.nvim_get_all_options_info()['modifiable'] then
+        vim.api.nvim_set_hl(0, 'CursorLine', { bg = default })
+        return
+      end
+      local mode = vim.api.nvim_get_mode().mode
+      local color = default
+      -- #vim.highlight.create("CursorLine", { guibg = "#3c3836" })
+      if mode == 'n' then
+        color = opts.normal
+      elseif mode == 'i' then
+        color = opts.insert
+      end
+      vim.api.nvim_set_hl(0, 'CursorLine', { bg = color })
+    end,
+  })
 end
 
 return M
